53a049acc03d31aa64b9d4e81b4aa2d4849a7eaa
nhs3108 <nhs3108@gmail.com>
2016-01-07 16:33:21 +0700

    Home layout

--- app/src/main/java/com/example/nhs3108/fels102/activities/HomeActivity.java
+++ app/src/main/java/com/example/nhs3108/fels102/activities/HomeActivity.java
@@ -1,10 +1,17 @@
 package com.example.nhs3108.fels102.activities;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.nhs3108.fels102.R;
 
 
 /**
  * Created by hongson on 30/12/2015.
  */
 public class HomeActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
 }
